8422902f18b0963559ac9d4af75661f12cd36017
zwp <1258271806@qq.com>
2018-07-14 10:31:35 +0800

    添加bell分配方案

--- src/recursion/bell_n_m.py
+++ src/recursion/bell_n_m.py
@@ -10,10 +10,28 @@ bell数， 将n个资源分到m个位置上
 分配方法
 例如：将n个元素的集合划分为若干子集
 
+bell 公式
+f(n,m)=f(n-1,m-1)+m*f(n-1,m);
 
+
+将{x}插入到(n-1,m-1)中 和(n-1,m)中
 '''
+
+def bell(n,m):
+    if m>n or m==0: return 0;
+    if n==1 or m==1 or m==n : return 1;
+    return bell(n-1,m-1)+m*bell(n-1,m);
+
+def run():
+    n=4;
+    m=3;
+    
+    print(bell(n,m));
+
+    pass;
 
 
 
 if __name__ == '__main__':
+    run();
     pass
