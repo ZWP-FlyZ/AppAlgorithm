033a90416b9e1bf3195578d58d5fa8319f4df24a
zwp <1258271806@qq.com>
2018-10-16 20:29:45 +0800

    在apriori中添加关联规则的可信度计算

--- src/machine_learn/apriori_frequentitem.py
+++ src/machine_learn/apriori_frequentitem.py
@@ -4,6 +4,7 @@ Created on 2018年10月16日
 
 @author: zwp12
 '''
+from sklearn.feature_selection.tests.test_base import support
 
 '''
 利用apriori算法发现调用记录中的频繁项集
@@ -37,8 +38,8 @@ def createCkp1(Ck,k):
         for j in range(i+1,setsize):
             L2 = list(Ck[j])[:k-2];
             if L1==L2:
-                res.append(sorted(Ck[i] | Ck[j]));
-    res = list(map(frozenset,res))
+                union = frozenset(sorted(Ck[i] | Ck[j]));
+                res.append(union);
     return res;
 
 def scanD(D,Ck,minsupt):
@@ -85,14 +86,46 @@ def apriori(dataset,minsupport=0.5):
 
 
 
+def calcConf(freqSet,H,supportData,brl,minConf=0.7):
+    pH=[];
+    for confeq in H:
+        delset = freqSet-confeq;
+        conf = supportData[freqSet]/supportData[delset];
+        if conf>minConf:
+            brl.append((delset,confeq,conf));
+            pH.append(confeq);
+    return pH;
+
+def rulesFromConseq(freqSet,H,supportData,brl,minConf=0.7):
+    m = len(H[0]);
+    if (len(freqSet))>(m+1):
+        hmpl = createCkp1(H, m+1);
+        hmpl = calcConf(freqSet,hmpl,supportData,brl,minConf);
+        if (len(hmpl)>1):
+            rulesFromConseq(freqSet,hmpl,supportData,brl,minConf);
+
+def generateRules(L,supportData,minConf=0.7):
+    ruleList=[];
+    for i in range(1,len(L)): #L[0]中是只包含一个元素的集合，这里选择元素不少于1的元素
+        for freqSet in L[i]:
+            H1 = [frozenset([item]) for item in freqSet];# 创建只包含一个元素集合
+            if i>1:
+                rulesFromConseq(freqSet,H1,supportData,ruleList,minConf);
+            else:
+                calcConf(freqSet,H1,supportData,ruleList,minConf);
+    return ruleList;
+
+
 def run():
-    data = [[1,2],[2,3,4],[1,2,5]];
+    data = [[1,3,4],[2,3,5],[1,2,3,5],[2,5]];
     
-    L,supt = apriori(data);
-    print(L[::-1]);
+    L,supt = apriori(data,0.5);
+    print(L);
     print(supt); 
     pass;
 
+    rulist= generateRules(L,supt,0.5);
+    print(rulist);
 
 if __name__ == '__main__':
     run();
